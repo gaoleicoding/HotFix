44dbe549dcda33a331b5fb4e0e1dfa3293464aaa
GL <gl@GaoLeideMacBook-Pro.local>
2019-04-01 00:04:08 +0800

    实现sophix

---
+++ hotfix-sample/sophix_demo/src/main/java/com/example/sophix/MyApplication.java
@@ -0,0 +1,35 @@
+package com.example.sophix;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.Keep;
+import android.util.Log;
+
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixApplication;
+import com.taobao.sophix.SophixEntry;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
+
+/**
+ * User: xingzhi.wap
+ * Date:16/5/17
+ */
+
+/**
+ * Sophix入口类，专门用于初始化Sophix，不应包含任何业务逻辑。
+ * 此类必须继承自SophixApplication，onCreate方法不需要实现。
+ * 此类不应与项目中的其他类有任何互相调用的逻辑，必须完全做到隔离。
+ * AndroidManifest中设置application为此类，而SophixEntry中设为原先Application类。
+ * 注意原先Application里不需要再重复初始化Sophix，并且需要避免混淆原先Application类。
+ * 如有其它自定义改造，请咨询官方后妥善处理。
+ */
+public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+    }
+
+}
